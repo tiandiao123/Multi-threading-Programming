6830e53c6bb4e3cd5d294d0fdcbbb9bdbdd031b4
Cuiqing Li <lixx3527@umn.edu>
2017-05-12 00:54:36 -0400

    add sleep java file

--- codes/SleepExample.java
+++ codes/SleepExample.java
@@ -3,9 +3,32 @@ import java.util.*;
 public class SleepExample{
    public static void main(String[] args){
 
+        MyThread thread1=new MyThread();
+        MyThread thread2=new MyThread();
+        thread1.start();
+        try {
+            thread1.join();
+        }catch(InterruptedException ie){
+            ie.printStackTrace();
+        }
 
+        thread2.start();
 
+   }
 
+   public static class MyThread extends Thread{
+       @Override
+       public void run(){
+
+          for(int i=0;i<10;i++){
+                System.out.println(i);
+                try{
+                    Thread.sleep(200);
+                }catch (InterruptedException ie){
+                    ie.printStackTrace();
+                }
+          }
+       }
    }
 
 }
