21ab8cf1452e9d5eca552e3dccf3283391fc5778
Cuiqing Li <lixx3527@umn.edu>
2017-05-14 18:01:43 -0400

    change server side of java

--- Server and Client/EchoClient.java
+++ Server and Client/EchoClient.java
@@ -10,15 +10,14 @@ public class EchoClient {
 
         if (args.length > 0)
            serverHostname = args[0];
-        System.out.println ("Attemping to connect to host "+ serverHostname + " on port 10007.");
+        System.out.println ("Attemping to connect to host "+ serverHostname + " on port 8005.");
 
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
 
        try {
-            // echoSocket = new Socket("taranis", 7);
-          echoSocket = new Socket(serverHostname, 10007);
+          echoSocket = new Socket(serverHostname, 8005);
           out = new PrintWriter(echoSocket.getOutputStream(), true);
           in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
        } catch (UnknownHostException e) {
@@ -33,7 +32,7 @@ public class EchoClient {
 	   String userInput;
        System.out.print ("input: ");
 	
-       while ((userInput = stdIn.readLine()) != null) {
+       while ((userInput = stdIn.readLine()) != null){
 	       out.println(userInput);
 	       System.out.println("echo: " + in.readLine());
            System.out.print ("input: ");
--- Server and Client/EchoServer.java
+++ Server and Client/EchoServer.java
@@ -1,5 +1,6 @@
 import java.net.*; 
 import java.io.*; 
+import java.util.*;
 
 //the following codes are based on https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
 
@@ -9,45 +10,43 @@ public class EchoServer
    { 
     ServerSocket serverSocket = null; 
 
-    try { 
-         serverSocket = new ServerSocket(10007); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Could not listen on port: 10007."); 
-         System.exit(1); 
-        } 
+    try{ 
+       serverSocket = new ServerSocket(8005); 
+    }catch (IOException e) {
+       System.err.println("Could not listen on port: 8005."); 
+       System.exit(1); 
+    } 
 
     Socket clientSocket = null; 
     System.out.println ("Waiting for connection.....");
 
-    try { 
-         clientSocket = serverSocket.accept(); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Accept failed."); 
-         System.exit(1); 
-        } 
+    try{ 
+        clientSocket = serverSocket.accept(); 
+    } catch (IOException e){
+        System.err.println("Accept failed."); 
+        System.exit(1); 
+    } 
 
     System.out.println ("Connection successful");
     System.out.println ("Waiting for input.....");
 
-    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                      true); 
-    BufferedReader in = new BufferedReader( 
-            new InputStreamReader( clientSocket.getInputStream())); 
+    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+    BufferedReader in = new BufferedReader( new InputStreamReader( clientSocket.getInputStream())); 
 
-    String inputLine; 
+    String inputLine;
+    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
 
-    while ((inputLine = in.readLine()) != null) 
-        { 
-         System.out.println ("Server: " + inputLine); 
-         out.println(inputLine); 
+    while ((inputLine = in.readLine()) != null) {
+        System.out.println ("Server: " + inputLine); 
+        System.out.println("type something to reply?:");
+        
+        String reply=scan.readLine();
 
-         if (inputLine.equals("Bye.")) 
-             break; 
-        } 
+        out.println(reply); 
+
+        if (inputLine.equals("Bye.")) 
+            break; 
+    } 
 
     out.close(); 
     in.close(); 
